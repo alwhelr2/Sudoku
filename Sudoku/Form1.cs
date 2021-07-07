6cf52328d4d9599bd1d7bda2c90ba40d39840648
alwhelr2 <awheeler121@gmail.com>
2021-07-07 15:26:55 -0500

    Drag-n-Drop

--- Sudoku/Form1.cs
+++ Sudoku/Form1.cs
@@ -12,9 +12,154 @@ namespace Sudoku
 {
     public partial class Form1 : Form
     {
+        Dictionary<Tuple<byte, byte>, Button> buttonKeys;
+        //Board gameBoard;
+        Tuple<bool, byte> drawImage;
+        Image[] images;
+
         public Form1()
         {
             InitializeComponent();
+
+            buttonKeys = new Dictionary<Tuple<byte, byte>, Button>();
+            //gameBoard = new Board();
+            //setButtons(gameBoard);
+            drawImage = Tuple.Create(false, (byte)0);
+            images = new Image[9];
+            images[0] = new Bitmap(Sudoku.Properties.Resources.s1);
+            images[1] = new Bitmap(Sudoku.Properties.Resources.s2);
+            images[2] = new Bitmap(Sudoku.Properties.Resources.s3);
+            images[3] = new Bitmap(Sudoku.Properties.Resources.s4);
+            images[4] = new Bitmap(Sudoku.Properties.Resources.s5);
+            images[5] = new Bitmap(Sudoku.Properties.Resources.s6);
+            images[6] = new Bitmap(Sudoku.Properties.Resources.s7);
+            images[7] = new Bitmap(Sudoku.Properties.Resources.s8);
+            images[8] = new Bitmap(Sudoku.Properties.Resources.s9);
+        }
+
+        private void setButtons(byte[,] board)
+        {
+            for (byte i = 0; i < 9; i++)
+                for (byte j = 0; j < 9; j++)
+                {
+                    Button b = getButtonFromIndices(i, j);
+                    b.Text = board[i, j].ToString();
+                }
+        }
+
+        private void setButton(byte row, byte col, byte value)
+        {
+            buttonKeys[Tuple.Create(row, col)].Text = value.ToString();
+        }
+
+        private Button getButtonFromIndices(byte row, byte col)
+        {
+            return buttonKeys[Tuple.Create(row, col)];
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawImage.Item1)
+                Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (drawImage.Item1)
+            {
+                Point local = this.PointToClient(Cursor.Position);
+                e.Graphics.DrawImage(images[drawImage.Item2], local);
+            }
+        }
+
+        private Button getMouseButton()
+        {
+            var formButtons = GetAll(this, typeof(Button));
+            foreach (Button b in formButtons)
+            {
+                if (b.ClientRectangle.Contains(b.PointToClient(Cursor.Position)))
+                {
+                    return b;
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<Control> GetAll(Control control, Type type)
+        {
+            var controls = control.Controls.Cast<Control>();
+
+            return controls.SelectMany(ctrl => GetAll(ctrl, type))
+                                      .Concat(controls)
+                                      .Where(c => c.GetType() == type);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)0);
+            Invalidate();
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)1);
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)2);
+        }
+
+        private void pictureBox4_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)3);
+        }
+
+        private void pictureBox5_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)4);
+        }
+
+        private void pictureBox6_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)5);
+        }
+
+        private void pictureBox7_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)6);
+        }
+
+        private void pictureBox8_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)7);
+        }
+
+        private void pictureBox9_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawImage = Tuple.Create(true, (byte)8);
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            Button button = getMouseButton();
+            if (button != null)
+            {
+                button.Text = (drawImage.Item2 + 1).ToString();
+
+            }
+
+            Console.WriteLine("STOP DRAWING");
+            drawImage = Tuple.Create(false, (byte)0);
+            Invalidate();
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawImage.Item1)
+            {
+                Invalidate();
+            }
         }
     }
 }
--- Sudoku/Properties/Resources.Designer.cs
+++ Sudoku/Properties/Resources.Designer.cs
@@ -8,9 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Sudoku.Properties
-{
+namespace Sudoku.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,52 +19,134 @@ namespace Sudoku.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sudoku.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s1 {
+            get {
+                object obj = ResourceManager.GetObject("s1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s2 {
+            get {
+                object obj = ResourceManager.GetObject("s2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s3 {
+            get {
+                object obj = ResourceManager.GetObject("s3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s4 {
+            get {
+                object obj = ResourceManager.GetObject("s4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s5 {
+            get {
+                object obj = ResourceManager.GetObject("s5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s6 {
+            get {
+                object obj = ResourceManager.GetObject("s6", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s7 {
+            get {
+                object obj = ResourceManager.GetObject("s7", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s8 {
+            get {
+                object obj = ResourceManager.GetObject("s8", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap s9 {
+            get {
+                object obj = ResourceManager.GetObject("s9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
